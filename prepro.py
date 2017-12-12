672321f794925c3ecac1cdf1b19610df40a85475
Marc Rudolph <marcdrudolph@gmail.com>
2017-12-12 11:16:31 -0500

    new python script

---
+++ prepro.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 10:54:00 2017
+
+@author: marc
+"""
+
+## Import all needed packages
+import glob
+import os
+import pdb
+import pandas
+import argparse
+
+def prepro(basedir):
+    print('Hello data in the path '+basedir)
+    
+def main():
+    basedir='/Users/marc/fmri_workshop/day01_fmriworkshop'
+    prepro(basedir)
+    
+main()
+    
+    
+  
